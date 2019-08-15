dadfba7608b08a27e41750c0048118db8d7e18c3
Yasha Ektefaie <yashaektefaie@Yashas-MacBook-Pro-3.local>
2019-08-15 07:13:22 -0700

    added testing

--- treatment_program_rcm.py
+++ treatment_program_rcm.py
@@ -82,31 +82,6 @@ def break_down_mutation(mutation):
 	else:
 		raise Exception('Unknown mutation format {}'.format(type_change_info))
 
-	# if(type_change_info[0] == 'SNP' and type_change_info[1] == 'CN'):
-	#     	gene_name, codonAA = type_change_info[5], type_change_info[4]
-	# elif(type_change_info[0] == 'SNP' and type_change_info[1] == 'P'):
-	#     	gene_name, codonAA = type_change_info[4], type_change_info[3]
-	#     	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-	# elif(type_change_info[0] == 'SNP' and type_change_info[1] == 'N'):
-	#     	gene_name, codonAA = type_change_info[4], type_change_info[3]
-	#     	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-	# elif(type_change_info[0] == 'SNP' and type_change_info[1] == 'I'):
-	#     	gene_name, codonAA= type_change_info[4], type_change_info[3]
-	#     	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','')
-	# elif(type_change_info[0] == 'INS'):
-	#     	gene_name, codonAA = type_change_info[4], type_change_info[3]
-	#     	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','')
-	# elif(type_change_info[0] == 'DEL'):
-	#     	gene_name, codonAA = type_change_info[4], type_change_info[3]
-	#     	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','')
-	# else:
-	# 	#So this is a non-standard type, but it may still be relevant so we can just use regex
-	# 	#for gene name we can always assume its in the same position
-	# 	gene_name = type_change_info[4]
-	# 	#However, for the codon position we use regex any numbers will be codon position
-	# 	codon_position = [i for i in re.findall('\d*', type_change_info[3]) if i][0]
-	# 	type_change = ''
-	print("{}\t{}\t{}\t{}".format(gene_name, codon_position, type_change, drug))
 	return Variant(gene_name, codon_position, type_change, drug=drug)  
 
 ###RUN CHECK MUTATION TEST i.e. see no failures
@@ -119,8 +94,10 @@ for vcf_file in vcf_files:
 		#print("{}\t{}".format([i for i in line.split('\t') if i in ['SNP','DEL','INS','LSP']], str(variant)))
 	count += 1
 
-def check_variant_commercial(variant):
-	drug, gene_name, codon_position, type_change = variant.drug, variant.gene_name, variant.codon_position, variant.type_change
+def check_variant_commercial(variant, drug):
+	gene_name, codon_position, type_change = variant.gene_name, variant.codon_position, variant.type_change
+	if(drug == 'ISONIAZID'):
+		drug = 'INH'
 	return_variable = False
 	if(drug == 'INH' and gene_name == 'katG' and codon_position == 315):
 		return_variable = True
@@ -140,311 +117,50 @@ def check_variant_commercial(variant):
 		return_variable = True
 
 	if(return_variable and type_change[0] == type_change[1]):
-		return True, 'synonymous'
+		return 'synonymous'
 	elif(return_variable and type_change[0] != type_change[1]):
-		return False, 'asynonymous'
+		return 'asynonymous'
 	else:
 		return False
 
-# def produce_variant(line, search = 0, search_drug = None):
-
-# 	if(search):
-# 		line = line.split('\t')
-# 		ID = line[5]
-		
-# 		#We do some sneaky stuff here -- if & we just split it up and recursively call this function on each part
-# 		#In effect we create two lines from one
-# 		if('&' in ID):
-# 			#print("OOOOHHH you got the speeccciialll caaassseeee")
-# 			multiple = ID.split('&')
-# 			for i in multiple:
-# 				copy = line[:]
-# 				copy[5] = i
-# 				if(produce_variant('\t'.join(copy), 1, search_drug)):
-# 					return True	
-# 			return False
-	
-# 		type_change_info = ID.split('_') 
-		
-# 		#print(line)
-# 		#print(type_change_info)
-#         	if(type_change_info[0] == 'SNP' and type_change_info[1] == 'CN' and 'LSP' not in type_change_info):
-#                 	gene_name, codonAA = type_change_info[5], type_change_info[4]
-# 			if('-' in codonAA):
-# 				codon_position = [i for i in re.findall('\d*', codonAA) if i]
-# 				type_change = ''
-# 			else:
-#                 		type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-#         	elif(type_change_info[0] == 'SNP' and type_change_info[1] == 'P'):
-#                 	gene_name, codonAA = type_change_info[4], type_change_info[3]
-#                 	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-#         	elif(type_change_info[0] == 'SNP' and type_change_info[1] == 'N'):
-#                 	gene_name, codonAA = type_change_info[4], type_change_info[3]
-#                 	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-#         	elif(type_change_info[0] == 'SNP' and type_change_info[1] == 'I' and 'DEL' not in type_change_info and 'INS' not in type_change_info):
-#                 	gene_name, codonAA = type_change_info[4], type_change_info[3]
-# 			if('-' in codonAA):
-# 				codon_position = [i for i in re.findall('\d*', codonAA) if i]
-# 				type_change = ''
-# 			else:
-#                 		type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-#         	elif(type_change_info[0] == 'INS' and type_change_info == 'F'):
-#                 	gene_name, codon_position = type_change_info[4], type_change_info[3]
-#                 	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','')
-#         	elif(type_change_info[0] == 'DEL' and type_change_info[1] == 'F'):
-#                 	gene_name, codon_position = type_change_info[4], type_change_info[3]
-#                 	type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','')
-#         	else:
-# 				#So this is a non-standard type, but it may still be relevant so we can just use regex
-# 				#for gene name we can always assume its in the same position
-# 				gene_name = type_change_info[4]
-# 				#However, for the codon position we use regex any numbers will be codon position
-# 				codon_position = [i for i in re.findall('\d*', type_change_info[3]) if i][0]
-# 				type_change = ''
-# 	        		#print("THERES SOMETHING ELSE LURKING HERE....RUN! {}".format(line))
-			
-# 			#return False
-# 		#print(line)
-# 		#print(type_change_info)
-# 		#if(len(codon_position) > 30 or '&' in ID):
-# 			#print("CODON MESSED UP: {}\t{}".format(line,codon_position))
-# 			#return False
-
-# 	if(not search):
-# 		line = line.replace('\n','')
-# 		data = line.split('_')
-# 		drug, type_change_info = data[0], data[1:]
-
-# 		if('_u' in line):
-# 			if('promoter' in line and '.' in line):
-# 				# print(line)
-# 				type_change_info = ['SNP'] + ['P'] + ['_'] + ['_'] + type_change_info
-# 				type_change_info[3] = type_change_info[5]
-# 				# print(type_change_info)
-# 			else:
-# 				# print(line)
-# 				type_change_info = ['SNP'] + ['CN'] + ['_'] + ['_'] + type_change_info
-# 				# print(type_change_info)
-# 		# type_change_info = type_resistance.split('_')
-# 		if(type_change_info[1] == 'CN'):
-# 			gene_name, codonAA = type_change_info[4], type_change_info[5]
-# 			type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1]
-# 		elif(type_change_info[1] == 'P'):
-# 			gene_name, codon_position = type_change_info[4], type_change_info[3]
-# 			if('.' in codon_position):
-# 				split = codon_position.split('.')
-# 				codon_position = split[1]
-# 				type_change = split[0]
-# 			if('.' in gene_name):
-# 				gene_name = 'promoter-'+gene_name.replace('.','-')
-# 			else:
-# 				gene_name = 'promoter-'+gene_name
-# 		elif(type_change_info[1] == 'N'):
-# 			gene_name, codonAA = type_change_info[4], type_change_info[3]
-# 			type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','') 
-# 			#type_change = None
-# 		elif(type_change_info[1] == 'I'):
-# 			gene_name, codon_position = type_change_info[4], type_change_info[3]
-# 			if('.' in codon_position):
-# 	                        split = codon_position.split('.')
-# 	                        codon_position = split[1]
-# 	                        type_change = split[0]
-# 			if('.' in gene_name):
-# 				gene_name = 'inter-'+gene_name.replace('.','-')
-# 		elif(type_change_info[0] == 'INS' and type_change_info[1] == 'F'):
-# 			gene_name, codonAA = type_change_info[4], type_change_info[3]
-# 			type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','') 
-# 			#type_change = None
-# 		elif(type_change_info[0] == 'DEL' and type_change_info[1] == 'F'):
-# 			gene_name, codonAA = type_change_info[4], type_change_info[3]
-# 			type_change,codon_position = codonAA[0]+codonAA[len(codonAA)-1], codonAA[1:len(codonAA)-1].replace('\n','') 
-# 			#type_change = None
-# 		else:
-# 			print("THERES SOMETHING ELSE LURKING HERE....RUN! {}".format(line))
-
-# 	"""
-
-# 	From presentation
-# 	INH katG codon 315
-# 	INH inhA promoter -15, -16, -8
-# 	RIF rpoB codon 505-534
-# 	AMINOGLYCOSIDES rrs 1401, 1402
-# 	AMINOGLYCOSIDES eis promoter -10 to -14, -37
-# 	FLQ gyrA 88-94
-# 	FLQ gyrB 500-541
-# 	"""
-# 	#THIS IS WHERE the info for commercial is stored -- we check if commercial!
-# 	def check(drug, gene_name, codon_position, search, type_change):
-# 		add = False
-# 		if(drug == 'INH' and gene_name == 'katG' and codon_position == 315):
-# 			drug = 'INH'
-# 			add = True
-# 		elif(drug == 'INH' and 'inhA' in gene_name and codon_position in [-15, -16,-8]):
-# 			drug = 'INH'
-# 			add = True
-# 		elif(drug == 'RIF'  and gene_name == 'rpoB' and codon_position in list(range(505-81,534-81))):
-# 			drug = 'RIF'
-# 			add = True
-# 		elif(drug in ['KAN', 'AMK','CAP', 'SLIS'] and gene_name == 'rrs' and codon_position in [1401,1402]):
-# 			drug = 'SLIS'
-# 			add = True
-# 		elif(drug in ['KAN','AMK','CAP', 'SLIS'] and gene_name == 'eis' and codon_position in [-10,-11,-12,-13,-14,-37]):
-# 			drug = 'SLIS'
-# 			add = True
-# 		#elif(drug in ['LEVO','FLQ'] and gene_name == 'gyrA' and codon_position in [88,89,90,91,92,93,94]):
-# 		elif(drug in ['LEVO', 'CIP','OFLX', 'FLQ'] and gene_name == 'gyrA' and codon_position in [88,89,90,91,92,93,94]):
-# 			drug = 'FLQ'
-# 			add = True
-# 		#elif(drug in ['LEVO','FLQ'] and gene_name == 'gyrB' and codon_position in list(range(500,541))):
-# 		elif(drug in ['LEVO', 'CIP','OFLX', 'FLQ'] and gene_name == 'gyrB' and codon_position in list(range(500,541))):
-# 			drug = 'FLQ'
-# 			add = True
-
-# 		if(add and search):
-# 			print("Checking: {}".format(type_change))
-# 			if(type_change[0] == type_change[1]):
-# 				print("BOIIIII DAMN WE FOUND IT A SYNONYMOUS AND WE WERE ABOUT TO APPROVVEEE")
-# 				print(type_change)
-# 			return True
-# 		elif(not add and search):
-# 			return False
-# 		return add
 
 
-# 	if(search and search_drug):
-# 		drug = search_drug
+file_resultat = open('commercial_results','w')
+file_resultat.format('drug\tmutation\tresistant\tsusceptible\tsynonymous\tasynonymous')
+for drug in ['ISONIAZID','RIF','SLIS','FQ']:
+	#memoization
+	store_results = {}
+	#Only look at strains with data
+	sub = strain_info[(strain_info['NO_DATA'] != 1) &(strain_info[drug] != -1)]
+	for index, row in sub.iterrows():
+		vcf = open(vcf_directory+'/'+row['strain'],'r')
+		print("{}\t{}".format(count, len(sub.iterrows())))
+		for line in vcf.readlines()[1:]:
+			mutation = [i for i in line.split('\t') if 'SNP' in i or 'INS' in i or 'DEL' in i or 'LSP' in i][0]
+			if(mutation in store_results):
+				if(store_results[mutation]):
+					file_resultat.write(store_results[mutation])
+			else:
+				commercial = check_variant_commercial(break_down_mutation(line), drug)
+				if(commercial):
+					if(row[drug] != 1):
+						if(commercial == 'synonymous'):
+							memoization[mutation] = "{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 0,1,1,0)
+							file_resultat.write("{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 0,1,1,0))
+						elif(commercial == 'asynonymous'):
+							memoization[mutation] = "{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 0,1,0,1)
+							file_resultat.write("{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 0,1,0,1))
+					else:
+						if(commercial == 'synonymous'):
+							memoization[mutation] = "{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 1,0,1,0)
+							file_resultat.write("{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 1,0,1,0))
+						elif(commercial == 'asynonymous'):
+							memoization[mutation] = "{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 1,0,0,1)
+							file_resultat.write("{}\t{}\t{}\t{}\t{}\t{}".format(drug, line.split('\t')[5], 1,0,0,1))
+				else:
+					memoization[mutation] = False
+		count += 1
 
-# 	add = False
-# 	codon_positionn = None
-# 	if(isinstance(codon_position, list)):
-# 		codon_positionn = [check(drug, gene_name, int(i), search, type_change) for i in codon_position]
-# 		if False in codon_positionn:
-# 			return False
-# 		else:
-# 			return True
-# 	else:
-# 		codon_position = int(codon_position)
-# 		add = check(drug, gene_name, codon_position, search, type_change)
-# 		if(not add):
-# 			return False
-# 		elif(add and search):
-# 			return True
-
-# 	if(add):
-# 		if(drug in ['LEVO','CIP','OFLX']):
-# 		#if(drug in ['LEVO']):
-# 			drug = 'FLQ'
-# 		elif(drug in ['KAN','AMK','CAP']):
-# 			drug = 'SLIS'
-# 		if(drug not in drug_to_variants):
-# 			drug_to_variants[drug] = []
-# 		else:
-# 			drug_to_variants[drug].append(line)
-
-
-# #Complex logic here -- basically we only add to drug_to_variants if its a commercial one that we already found
-# for line in [i for i in list(res.columns)[1:] if 'unknown' not in i and 'embA' not in i and 'PAS' not in i and 'PZA' not in i]:
-# 	produce_variant(line)
-
-
-# variants_unique_WGS = set()
-# strains_analyzed = set()
-# commercial
-# WGS = 
-
-
-# for d in ['INH','RIF','SLIS','FLQ']:
-# #for d in ['FLQ']:
-# 	"""
-# 		FLQ includes CIP, OFLX, LEVO
-# 		SLIS includes KAN, AMK, CAP
-
-# 	"""
-# 	if(d not in drug_to_variants):
-# 		print("{} not detected at all".format(d))
-# 	else:
-# 		mutations_interest = drug_to_variants[d]
-# 		print(mutations_interest)
-# 		if(d == 'INH'):
-# 			d = 'ISONIAZID'
-# 		sub = combined[combined[d] == 1].copy()
-# 		#sub = combined[combined['LEVOFLOXACIN'] == 1].copy()
-# 		total = 0
-
-# 		total_count   = len(sub.index)
-# 		count = 0
-# 		for index, row in sub.iterrows():
-# 			det = False
-# 			strains_analyzed.add(row['strain'])
-
-# 			def perform_commercial_test(row, mutations_interest):
-# 				#Step 1: Search for commercial we already found (i.e. already in list, we already looked for it)
-# 				for mutation in mutations_interest:
-# 					if(int(row[str(mutation)]) == 1):
-# 						det = True
-# 						file.write('{}\t{}\t{}\n'.format(row['strain'], str(mutation), 'commercial'))
-# 						break
-
-# 				#Step 2: If we found no commercial, then search the vcf for a mutation in the genes we didn't already find but are tested by commercial drugs
-# 				if(not det):
-# 					#Go through lilne by line in vcf
-# 					vcf_file = [i for i in vcf_files if row['strain']+'.var'== i]
-# 					if(len(vcf_file) > 1):
-# 						raise Exception('More than one vcf file found for {}'.format(row['strain']))
-# 					elif(not vcf_file):
-# 						raise Exception('No vcf file found for {}'.format(row['strain']))
-					
-# 					name = vcf_file[0]
-# 					vcf = open(vcf_directory+'/'+name, 'r')
-# 					for line in vcf.readlines()[1:]:
-# 						if(produce_variant(line, 1, d)):
-# 							print(line)
-# 							print("wow good thing we did this found a commercial!")
-# 							det = True
-# 							file.write('{}\t{}\t{}\n'.format(row['strain'], line.split('\t')[5], 'commercial'))
-# 							break
-
-# 			def perform_WGS_test(row):
-# 				if(d == 'ISONIAZID'):
-# 					d = 'INH'
-# 				WGS_only = []
-# 				for i in list(sub.columns):
-# 					if i not in mutations_interest:
-# 						if 'unknown' not in i:
-# 							if '_' in i:
-# 								if(d == 'INH' or d == 'RIF'):
-# 									if(d in i):
-# 										WGS_only.append(i)
-# 								else:
-# 									if(d == 'FLQ'):
-# 										if('CIP' in i or 'OFLX' in i or 'LEVO' in i):
-# 										#if('LEVO' in i):
-# 											WGS_only.append(i)
-# 									elif(d == 'SLIS'): 
-# 										if('KAN' in i or 'AMK' in i or 'CAP' in i):
-# 											WGS_only.append(i)
-
-# 				for mutation in WGS_only:
-# 					if(int(row[str(mutation)]) == 1):
-# 						variants_unique_WGS.add(row['strain'])
-# 						det = True
-# 						file.write('{}\t{}\t{}\n'.format(row['strain'], str(mutation), 'WGS'))
-# 						break
-
-# 			#Step 4: If after all that commercial and WGS can't find it then the strain is undetected with current information
-# 			if(not det):
-# 				undetected += 1
-
-# 			count += 1
-# 			print("{}\t{}".format(count, total_count))
-# 			total += 1
-# 		total = float(total)
-# 		print("DRUG: {} \t COMMERCIAL DETECTED: {} \t TOTAL: {} \t PERCENT DETECTED: {}".format(d, detected_commerical, total, detected_commerical/total))
-# 		print("DRUG: {} \t WGS DETECTED: {} \t TOTAL: {} \t PERCENT DETECTED: {}".format(d, detected_WGS, total, detected_WGS/total))
-# 		print("DRUG: {} \t UNDETECTED: {} \t TOTAL: {} \t PERCENT UNDETECTED: {}".format(d, undetected, total, undetected/total))
-
-
-# print("TOTAL NUMBER ISOLATES RESISTANCE IDENTIFIED VIA WGS: {} \ {}".format(len(list(variants_unique_WGS)),len(list(strains_analyzed))))
 
 
 
--- treatment_program_rcm_helper.py
+++ treatment_program_rcm_helper.py
@@ -22,6 +22,16 @@ class Variant:
 		return "{}_{}".format(self.gene_name, return_string)
 
 def annotate(df):
+	"""
+	These next definitions annotate the strain_info.tsv file with drug categories we use later on in analysis
+
+	Conditions for various categories 
+	1 ==> We have the data that shows that this isolate should be in this category
+	0 ==> We have the data that shows that this isolate should NOT be in this category
+	-1 ==> We don't have enough data to say anything :(
+
+	"""
+
 	#Rifamycin category requires that isolate be resistant to either rifabutin or rifampicin
 	def RIFAMYCIN(row):
 		if(row['RIFABUTIN'] == 1 or row['RIFAMPICIN'] == 1):
@@ -42,7 +52,7 @@ def annotate(df):
 		else:
 			return -1
 
-	df['FLQ'] = df.apply(lambda row: FLUOROQUINOLONES(row),axis=1)
+	df['FQ'] = df.apply(lambda row: FLUOROQUINOLONES(row),axis=1)
 
 	#Slis category requires that isolate be resistant to either kanamycin or amikacin or capreomycin
 	def SLIS(row):
@@ -87,6 +97,86 @@ def annotate(df):
 			return -1
 
 	df['PAS'] = df.apply(lambda row: PARA_AMINOSALICYLIC_ACID(row),axis=1)
+
+	#To be MDR the isolate has to be resistant to both Isoniazid and RIF
+	def MDR(row):
+		if(row['RIF'] == 1 and row['ISONIAZID'] == 1):
+			return 1
+		elif(row['RIF'] == -1 or row['ISONIAZID'] == -1):
+			return -1
+		else:
+			return 0
+
+	df['MDR'] = df.apply(lambda row: MDR(row),axis=1)
+
+	#To be XDR the isolate has to be MDR and resistant to Fluoroquinolones and SLIS
+	def XDR(row):
+		if(row['MDR'] == 1 and row['FQ'] == 1 and row['SLIS'] == 1):
+			return 1
+		elif(row['MDR'] == -1 or row['FQ'] == -1 or row['SLIS'] == -1):
+			return -1
+		else:
+			return 0
+
+	df['XDR'] = df.apply(lambda row: XDR(row),axis=1)
+
+	#For these next categories we only look at isolates that have data on isoniazid and rifamycin (i.e df[(df['ISONIAZID'] > -1 ) & (df['RIFAMYCIN'] > 1)])
+	#To be susceptible the isolate has to have data on isoniazid and rif and then not have any data that shows it is resistant
+	def S(row):
+		if(row['ISONIAZID'] > -1 and row['RIF'] > -1):
+			if(row['ISONIAZID'] < 1 and row['RIF'] < 1 and row['FQ'] < 1 and row['SLIS'] < 1 and row['PAS'] < 1 and row['CYS'] < 1 and row['ETH'] < 1 and row['ETHAMBUTOL'] < 1 and row['STREPTOMYCIN'] < 1 and row['PYRAZINAMIDE'] < 1):
+				return 1
+			else:
+				return 0
+		else:
+			return -1
+
+	df['S'] = df.apply(lambda row: S(row),axis=1)
+
+	#For Isoniazid mono resistant we look at isolates which have data on isoniazd and rif -- if the isolate has this data we then make sure it is only resistant to INH and susceptible to rif 
+	#and susceptible to any other drug the isolate was tested for except strep where we allow it to be resistant to both INH and strep and still be INH mono
+	def INH_MONO(row):
+		if(row['ISONIAZID'] > -1 and row['RIF'] > -1):
+			if(row['ISONIAZID'] == 1 and row['RIF'] == 0 and row['FQ'] < 1 and row['SLIS'] < 1 and row['PAS'] < 1 and row['CYS'] < 1 and row['ETH'] < 1 and row['ETHAMBUTOL'] < 1 and row['STREPTOMYCIN'] <= 1 and row['PYRAZINAMIDE'] < 1):
+				return 1
+			else:
+				return 0
+		else:
+			return -1
+
+	df['INH_MONO'] = df.apply(lambda row: INH_MONO(row),axis=1)
+
+	#Similar logic to the INH Mono except now we require that is only be resistant to Strep and susceptible to all other drugs
+	def STREP_MONO(row):
+		if(row['ISONIAZID'] > -1 and row['RIF'] > -1):
+			if(row['ISONIAZID'] == 0 and row['RIF'] == 0 and row['FQ'] < 1 and row['SLIS'] < 1 and row['PAS'] < 1 and row['CYS'] < 1 and row['ETH'] < 1 and row['ETHAMBUTOL'] < 1 and row['STREPTOMYCIN'] == 1 and row['PYRAZINAMIDE'] < 1):
+				return 1
+			else:
+				return 0
+		else:
+			return -1
+
+	df['STR_MONO'] = df.apply(lambda row: STREP_MONO(row),axis=1)
+
+	#Similar logic to the previous 2 except now we check if the isolate is not INH mono, strep mono, xdr, or mdr and it is still resistant to some drug then it is other resistant
+	def OTHER_R(row):
+		if(row['ISONIAZID'] > -1 and row['RIF'] > -1):
+			if((row['INH_MONO'] < 1 and row['STR_MONO'] < 1 and row['XDR'] < 1 and row['MDR'] < 1 ) and (row['ISONIAZID'] == 1 or row['RIF'] == 1 or row['FQ'] == 1 or row['SLIS'] == 1 or row['PAS'] == 1 or row['CYS'] == 1 or row['ETH'] == 1 or row['ETHAMBUTOL'] == 1 or row['STREPTOMYCIN'] == 1 or row['PYRAZINAMIDE'] == 1)):
+				return 1
+			else:
+				return 0
+		else:
+			return -1
+
+	df['OTHER_R'] = df.apply(lambda row: OTHER_R(row),axis=1)
+
+	#Find if an entry has no resistance data
+	def NO_DATA(row):
+		if(row['S'] < 0 and row['MDR'] < 0 and row['INH_MONO'] < 0 and row['STR_MONO'] < 0 and row['OTHER_R'] < 0 and row['XDR'] < 0):
+			return 1
+		return 0
+
+	df['NO_DATA'] = df.apply(lambda row: NO_DATA(row),axis=1)
 	return df.copy()
 
 
